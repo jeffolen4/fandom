037b255cc4636b08496c554d0e42514b8cc1b133
LEARN01 <learn01@LEARN01.local>
2015-01-28 16:39:38 -0800

    added madlib

--- fandom.rb
+++ fandom.rb
@@ -15,4 +15,18 @@ class Fandom < Sinatra::Application
     erb(:merch)
   end
 
+  get '/madlib-form' do
+    erb(:form)
+  end
+
+  get '/madlib-story' do
+    @person1 = params.fetch('person1', "{no person1}")
+    @person2 = params.fetch('person2', "{no person2}")
+    @animal = params.fetch('animal', "{no animal}")
+    @exclamation = params.fetch('exclamation', "{no explamation}")
+    @verb = params.fetch('verb', "{no verb}")
+    @noun = params.fetch('noun', "{no noun}")
+    erb(:story)
+  end
+
 end
